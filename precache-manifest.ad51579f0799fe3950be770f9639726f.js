38bac7ff3919c2d274c6e2329a6391c9c6e66c30
DESKTOP-5D3DQ0J\usuario <ssaavedra@soaint.com.pe>
2020-10-16 03:51:10 -0500

    Updates

---
+++ precache-manifest.ad51579f0799fe3950be770f9639726f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b6fa039eb21a4d40fd4dde8e742129",
+    "url": "/portafolio-personal/index.html"
+  },
+  {
+    "revision": "a3972e5aae56cfaf9db8",
+    "url": "/portafolio-personal/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "f146624b8cb599c66be7",
+    "url": "/portafolio-personal/static/css/main.39639de3.chunk.css"
+  },
+  {
+    "revision": "a3972e5aae56cfaf9db8",
+    "url": "/portafolio-personal/static/js/2.6d4b3e54.chunk.js"
+  },
+  {
+    "revision": "dd798cb644566625dfa98183c599779e",
+    "url": "/portafolio-personal/static/js/2.6d4b3e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f146624b8cb599c66be7",
+    "url": "/portafolio-personal/static/js/main.0a066b7f.chunk.js"
+  },
+  {
+    "revision": "f43caeb227a57043c5aa",
+    "url": "/portafolio-personal/static/js/runtime-main.9634ec0d.js"
+  }
+]);
